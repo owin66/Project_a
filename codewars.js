370219332c8755841dfa56290e1e0e942300e7f4
owin66 <78900270+owin66@users.noreply.github.com>
2022-07-11 22:12:38 +0300

    1

--- codewars.js
+++ codewars.js
@@ -161,3 +161,17 @@ function makeNegative(num) {
           return arr;
         }, [0, 0]);
       }
+2 variant
+      if (input == null || input.length == 0)
+      return [];
+    var positive = 0;
+    var negative = 0;
+    for (var i=0, l=input.length; i<l; ++i)
+    {
+      if (input[i] > 0)
+        ++ positive;
+      else
+        negative += input[i];
+    }
+    
+    return [positive, negative];
