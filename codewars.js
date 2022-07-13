cbd37f31a8bb694a7328f7cd559e9856afcdb6a6
owin66 <78900270+owin66@users.noreply.github.com>
2022-07-13 20:14:21 +0300

    great

--- codewars.js
+++ codewars.js
@@ -190,3 +190,28 @@ function makeNegative(num) {
     function stray(numbers) {
       return numbers.reduce((x, y) => x ^ y);
     }
+// напишите функцию которая будет приветствоать человека на его языке
+    function greet(language) {
+      var GreetingsDB = {
+          english: 'Welcome',
+          czech: 'Vitejte',
+          danish: 'Velkomst',
+          dutch: 'Welkom',
+          estonian: 'Tere tulemast',
+          finnish: 'Tervetuloa',
+          flemish: 'Welgekomen',
+          french: 'Bienvenue',
+          german: 'Willkommen',
+          irish: 'Failte',
+          italian: 'Benvenuto',
+          latvian: 'Gaidits',
+          lithuanian: 'Laukiamas',
+          polish: 'Witamy',
+          spanish: 'Bienvenido',
+          swedish: 'Valkommen',
+          welsh: 'Croeso'
+        }, 
+        defaultLanguage = 'english';
+        return GreetingsDB[language] || GreetingsDB[defaultLanguage];
+      }
+      
