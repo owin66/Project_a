fd6ce96e4ec3541fbd2e01c1356770cf5ed54784
owin66 <78900270+owin66@users.noreply.github.com>
2022-07-20 19:22:02 +0300

    accum_v2

--- codewars.js
+++ codewars.js
@@ -382,3 +382,7 @@ function removeSmallest(numbers) {
     .split('')
     .map((el,i)=>`${el}${el.repeat(i).toLowerCase()}`).join('-')
     }
+    // v2
+    function accum1(s) {
+      return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+    }
