2d4fdecc6e109c9bc4e4a764bfe17d1c3ccf8f42
owin66 <78900270+owin66@users.noreply.github.com>
2022-08-23 07:19:00 +0300

    refactro

--- codewars.js
+++ codewars.js
@@ -526,6 +526,7 @@ function isIsogram (str) {
   return !str || (str.length === new Set(str.toLowerCase()).size);
 }
 
+
 //найти целый квадрат
 function findNextSquare(sq) {
   return Math.sqrt(sq)%1? -1 : Math.pow(Math.sqrt(sq)+1,2);
