9a546c093554fbd1dbfb81235d69779e583e20df
owin66 <78900270+owin66@users.noreply.github.com>
2022-12-04 15:51:51 +0200

    author

--- codewars.js
+++ codewars.js
@@ -650,3 +650,32 @@ const getActiveUsers = (users) => {
 const getInactiveUsers = (users) => {
    return users.filter(el=>el.isActive === false).map(el=>el)
 };
+
+// Используя метод find() дополни код так, чтобы:
+
+// В переменной bookWithTitle получился объект книги название которой (свойство title) совпадает со значением переменной BOOK_TITLE.
+// В переменной bookByAuthor получился объект книги автор который (свойство author) совпадает со значением переменной AUTHOR.
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+];
+const BOOK_TITLE = 'The Dream of a Ridiculous Man';
+const AUTHOR = 'Robert Sheckley';
+// Change code below this line
+
+const bookWithTitle = books.find((el)=>el.title === BOOK_TITLE);
+const bookByAuthor = books.find((el)=>el.author === AUTHOR);
