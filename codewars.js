cb188a5d333004e3528496fc3a8a146d0f2bb544
owin66 <78900270+owin66@users.noreply.github.com>
2022-12-04 16:29:24 +0200

    reduce_2

--- codewars.js
+++ codewars.js
@@ -684,3 +684,22 @@ const bookByAuthor = books.find((el)=>el.author === AUTHOR);
 const getUserWithEmail = (users, email) => {
   return users.find((el)=>el.email===email)
 };
+// Дополни функцию isEveryUserActive(users) так, чтобы она проверяла все ли пользователи сейчас активны (свойство isActive) и возвращала true или false.
+const isEveryUserActive = (users) => {
+   return users.every(el=>el.isActive === true ? true : false)
+};
+
+
+const players = [
+  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+  { name: "Poly", playtime: 469, gamesPlayed: 2 },
+  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+];
+// Change code below this line
+// Нашему сервису необходимо рассчитать среднее время проведённое в одной игре для каждого игрока, и получить общую сумму этих времён.
+//  Рассчитать время для каждого из игроков, можно разделив его время(свойство playtime) на количество игр(свойство gamesPlayed).
+
+const totalAveragePlaytimePerGame = players.reduce((total, player) => {
+  return total + player.playtime/player.gamesPlayed
+},0);
