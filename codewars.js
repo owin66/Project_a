a108929deca51935fc711876ace452636868ede3
owin66 <78900270+owin66@users.noreply.github.com>
2022-12-13 00:21:21 +0200

    цц

--- codewars.js
+++ codewars.js
@@ -706,5 +706,9 @@ const totalAveragePlaytimePerGame = players.reduce((total, player) => {
 const etotalAveragePlaytimePerGame = players.reduce((total, player) => {
   return total + player.playtime/player.gamesPlayed
 },0);
+// Дополни функцию getInactiveUsers(users) так, чтобы она возвращала массив неактивных пользователей, значение свойства isActive которых false.
+const getInactiveUsers = (users) => {
+   return users.filter(el=>el.isActive === false).map(el=>el)
+};
 
 
